6f501a0dcde056159c4a9b67135383b7abe5ffa5
Sauravraj0995 <47918066+Sauravraj0995@users.noreply.github.com>
2019-03-23 14:52:50 +0530

    Add files via upload

---
+++ flight.js
@@ -0,0 +1,34 @@
+var uri;
+var object;
+var value;
+var number = 1;
+var cont;
+{
+
+var txt = '{"origin": "Bangalore","Date":"dd/mm/yyyy","Search":"","AirlineCode":"INDIGO","Flightnumber":3301,"DepartureTime":"19:20","Destination":"NewDehi","ArrivalTime":"22:20","Duration":"0hrs33min","Departure":"Bangalore","Stops":"1 stop"}'
+var obj = JSON.parse(txt);
+
+}
+
+
+     
+      
+       
+        
+            //var txt = '{"origin": "Bangalore","Date":"dd/mm/yyyy","Search":"","AirlineCode":"INDIGO","Flightnumber":3301,"DepartureTime":"19:20","Destination":"NewDehi","ArrivalTime":"22:20","Duration":"0hrs33min","Departure":"Bangalore"}'
+          
+
+          
+            
+              
+             
+            
+            
+
+      
+          
+        
+    
+  
+     
+
